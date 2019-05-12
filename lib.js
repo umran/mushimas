8f3ae139283f230f81ea11e3820b5b596a92c409
Umran Hussein <umran.hussein@gmail.com>
2019-05-12 03:53:31 -0700

    export graphql and mongoose methods

--- lib.js
+++ lib.js
@@ -1,4 +1,6 @@
 exports.buildBackend = require('./src/buildBackend')
 exports.buildGraphql = require('./src/buildGraphql')
 exports.validator = require('./src/validator')
+exports.graphql = require('./src/graphql')
+exports.mongoose = require('./src/mongoose')
 exports.elasticsearch = require('./src/elasticsearch')
